e4d654e25ebde4ff3d4091e8150d54638bf7da79
Hariharan s <158845852+hari-0805@users.noreply.github.com>
2026-05-19 15:50:27 +0530

    Delete movieweb directory

--- movieweb/src/components/ThemeToggle.jsx
+++
@@ -1,9 +0,0 @@
-function ThemeToggle({ darkMode, setDarkMode }) {
-  return (
-    <button className="theme-toggle-btn" onClick={() => setDarkMode(!darkMode)}>
-      {darkMode ? "☀️" : "🌙"}
-    </button>
-  );
-}
-
-export default ThemeToggle;
